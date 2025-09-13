7e4c4b71b7c1cb669886228bd7042babe4954c03
encloinc <jelch2002@gmail.com>
2025-09-13 17:52:45 +0000

    chore: add gitignore

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-cargo run -- --metashrew-db-path ~/mainnet-backup-913812-metashrew-8.8.4 --port 8080 --endpoint /traces
+cargo run -- --metashrew-db-path /data/.metashrew/mainnetnew884  --port 5777 --endpoint /traces
